304b3745938877b3df8d93213c95e161fc01c4bb
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-07-09 15:20:35 -0700

    css and dep

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#ffffff",
+        foreground: "#000000",
+        darkBackground: "#000000",
+        darkForeground: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
